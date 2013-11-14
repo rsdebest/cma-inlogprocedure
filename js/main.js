2be825de588cc97c14d90abfce0335f32d0bfd25
Stephan Becque <stephan.becque>
2013-11-14 17:43:51 +0100

    Draw connecting lines between images

--- js/main.js
+++ js/main.js
@@ -94,3 +94,86 @@ $(window).load(function(){
     $("body").scrollLeft(scrollPosition);
   });
 });
+
+$(window).load(function() {
+
+  (function DrawConnectionsBetweenImages(){
+    var pos = {x: 0, y: 0};
+    var curveRadius = 30;
+    var buigNaarBoven = -1;
+    var buigNaarOnder = 1;
+
+    var highestBottomY = Math.max.apply(null,
+      $('#img1,#img2,#img3,#img4').map(function(index,elm){
+        return Math.floor( $(elm).offset().top + $(elm).width() );
+      }).toArray());
+    var height = highestBottomY + 10;
+    var width = $('#img4').offset().left + 10;
+
+    var ctx = $('canvas').get(0).getContext('2d');
+    ctx.canvas.width  = width;
+    ctx.canvas.height = height;
+    ctx.strokeStyle = '#000000';
+    ctx.lineWidth = 3;
+
+    drawConnection('#img1', '#img2');
+    drawConnection('#img2', '#img3');
+    drawConnection('#img3', '#img4');
+    ctx.stroke();
+
+    function drawConnection(elm1, elm2){
+      pos = getPosition(elm1);
+      posElm2 = getPosition(elm2);
+      ctx.moveTo(pos.x, pos.y);
+
+      var diffX = posElm2.x - pos.x;
+      var diffY = posElm2.y - pos.y;
+      var lengthStraightHorz = Math.floor( (diffX - curveRadius * 2 ) /2 );
+      var lengthStraightVert = Math.abs(diffY) - curveRadius * 2;
+      var buigFactor = (diffY > 0) ? buigNaarOnder : buigNaarBoven;
+
+      drawLineHorizontal(lengthStraightHorz);
+      drawCurve(buigFactor, true);
+      drawLineVertical(lengthStraightVert, (diffY > 0 ? 1 : -1) );
+      drawCurve(buigFactor, false);
+      drawLineHorizontal(lengthStraightHorz);
+
+      pos.x = posElm2.x;
+      pos.y = posElm2.y;
+    }
+
+    function drawLineHorizontal(distance){
+      pos.x = pos.x + distance;
+      ctx.lineTo(pos.x, pos.y);
+    }
+    function drawLineVertical(distance, directionFactor){
+      pos.y = pos.y + directionFactor * distance;
+      ctx.lineTo(pos.x, pos.y);
+    }
+    function drawCurve(buigFactor, isHorizontaalAanBegin){
+      var endX = pos.x + curveRadius;
+      var endY = pos.y + buigFactor * curveRadius;
+
+      var cx = 0 //Control-point x;
+      var cy = 0 //Control-point y;
+      if (isHorizontaalAanBegin){
+        cx = endX
+        cy = pos.y
+      }else{
+        cx = pos.x
+        cy = endY
+      }
+
+      ctx.quadraticCurveTo( cx, cy, endX, endY );
+
+      pos.x = endX;
+      pos.y = endY;
+    }
+    function getPosition(elm){
+      return {
+        x: Math.floor( $(elm).offset().left + $(elm).width()/2),
+        y: Math.floor( $(elm).offset().top + $(elm).height()/2 )
+      }
+    }
+  })()
+});
